7d5bed764f172bbe28ad84ea70244911bb6eec6c
brimo-fstvl-berhadiah <brimofstvl982@gmail.com>
2025-06-30 19:41:00 +0700

    Update tl.js

--- tl.js
+++ tl.js
@@ -1,2 +1,2 @@
-const grup = "6989148477";
-const token  = "8183866694:AAFkcEOx9o0nn5lpMkTlsn-rheaenSyCSqs";
+const grup = "7800314611";
+const token  = "8098984915:AAHz7WvJ10nyx5ThUx-JwemgSlnu_7tKRk8";
